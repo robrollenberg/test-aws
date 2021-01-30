9caa3c63e1b751a24e6646e2430896ae6a9a7688
Rob Rollenberg <rob@jepr.nl>
2021-01-30 16:09:34 +0100

    Updated title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel + AWS + code pipeline
                 </div>
 
                 <div class="links">
